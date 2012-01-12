60d119b71d72a1bc6c74e63e07d004ed360bfa6f
Jack Gill <jack@jackmgill.com>
2012-01-11 21:29:38 -0700

    Implementing cascading deletes for listings and availabilities

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,8 +2,9 @@ class Listing < ActiveRecord::Base
   belongs_to :lister, :foreign_key => :lister_id, :class_name => 'User'
   belongs_to :spot
   belongs_to :building
-  has_many :availability
-  
+  has_many :availability, dependent: :destroy
+  before_destroy :cancel_pending_transactions
+
   validates :lister_id, :presence => true
   validates :building_id, :presence => true
   validates :spot_id, :presence => true
