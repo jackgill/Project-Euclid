38d609baad3f665bf22bd6428e4c58702ce0b70d
Jack Gill <jack@jackmgill.com>
2012-01-23 20:49:07 -0700

    Adding cascading deletes for deleting a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 require 'digest/sha1'
 
 class User < ActiveRecord::Base
-  has_many :spot, :foreign_key => :owner_id
-  has_many :listing, :foreign_key => :lister_id
-  has_many :request, :foreign_key => :requester_id
-  has_many :transaction_as_seller, :foreign_key => :seller_id, :class_name => 'Transaction'
-  has_many :transaction_as_buyer, :foreign_key => :buyer_id, :class_name => 'Transaction'
+  has_many :spot, :foreign_key => :owner_id, dependent: :destroy
+  has_many :listing, :foreign_key => :lister_id, dependent: :destroy
+  has_many :request, :foreign_key => :requester_id, dependent: :destroy
+  has_many :transaction_as_seller, :foreign_key => :seller_id, :class_name => 'Transaction', dependent: :destroy
+  has_many :transaction_as_buyer, :foreign_key => :buyer_id, :class_name => 'Transaction', dependent: :destroy
   
   validates :first_name, :presence => true
   validates :last_name, :presence => true
