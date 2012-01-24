47245b0bf316f20c56730d5f51ed57aad49fa680
Jack Gill <jack@jackmgill.com>
2012-01-23 23:36:28 -0700

    refactoring availabilities_controller to use message()

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -122,12 +122,10 @@ class AvailabilitiesController < ApplicationController
     # attempt to rent the spot
     respond_to do |format|
       if @availability.rent(start_date, end_date, buyer, seller)
-        flash[:notice] = 'Congratulations, you have successfully reserved this spot'
-        format.html { redirect_to controller: 'home', action: 'confirmation' }
+        format.html { message('Congratulations, you have successfully reserved this spot') }
         format.json { render json: @availability, status: :created, location: @listing }
       else
-        # TODO: do we need a "negation" page to redirect to?
-        format.html { redirect_to @availability, notice: 'Oh no, some one else has rented this spot whilst you were dilly-dallying' }
+        format.html { message('Oh no, some one else has rented this spot whilst you were dilly-dallying') }
         format.json { render json: @availability, status: :already_taken, location: @listing }
       end
     end
