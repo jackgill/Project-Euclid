6a4b9c1562f2f0327dcad26b9e66829e06556c80
Jack Gill <jack@jackmgill.com>
2012-02-20 17:58:10 -0700

    requiring admin to view admin or jump pages

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,6 @@
 class AdminController < ApplicationController
+  before_filter :require_admin
+  
   def index
   end
 
@@ -16,5 +18,13 @@ class AdminController < ApplicationController
     reset_session
     flash[:notice] = 'Session Cleared'
     redirect_to controller: 'home', action: 'splash'
+  end
+
+  def require_admin
+    unless @user && @user.is_admin
+      session[:requested_path] = request.parameters
+      flash[:notice] = "You must be an admin to view this page"
+      redirect_to :action => 'login', :controller => 'account'
+    end
   end
 end
